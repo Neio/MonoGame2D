0e7af4b0e7996494b6bdf7bd707bcbcf44717509
Neio <neio.zhou@gmail.com>
2012-05-29 17:15:38 +0800

    cs files

--- MonoGame2D/MonoGame2D/Node.cs
+++ MonoGame2D/MonoGame2D/Node.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace MonoGame2D
 {
     public class Node
     {
 
+        #region Childen
+
         public Node Parent { get; internal set; }
 
+        ContentManager _context = null;
         List<Node> _children = new List<Node>();
 
         public void AddChild(Node Node)
@@ -20,8 +24,9 @@ namespace MonoGame2D
             {
                 Node.Parent.RemoveChild(Node);
             }
-
             _children.Add(Node);
+            Node.Parent = this;
+            Node.LoadContents(_context);
         }
 
         public void RemoveChild(Node Node)
@@ -29,8 +34,28 @@ namespace MonoGame2D
             if (_children.Contains(Node))
             {
                 Node.Parent = null;
+                Node.UnloadContents();
                 _children.Remove(Node);
             }
+        }
+
+        public void RemoveAllChildren()
+        {
+            foreach (Node node in _children.ToArray())
+            {
+                RemoveChild(node);
+            }
+        }
+        #endregion
+
+        public virtual void LoadContents(ContentManager Context)
+        {
+            _context = Context;
+        }
+
+        public virtual void UnloadContents()
+        { 
+            
         }
 
         public virtual void Draw(GraphicsDevice graphic, GameTime TimeDelta)
@@ -40,7 +65,7 @@ namespace MonoGame2D
 
         public virtual void DrawContent(GraphicsDevice graphic, GameTime TimeDelta)
         { 
-        
+            
         }
 
         public virtual void DrawChildren(GraphicsDevice graphic, GameTime TimeDelta)
---
+++ MonoGame2D/MonoGame2D/Scene.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame2D
+{
+    public class Scene : Game
+    {
+        List<Node> _children = new List<Node>();
+
+        public void AddChild(Node Node)
+        {
+            if (Node.Parent != null)
+            {
+                Node.Parent.RemoveChild(Node);
+            }
+            _children.Add(Node);
+            Node.LoadContents(Content);
+        }
+
+        public void RemoveChild(Node Node)
+        {
+            if (_children.Contains(Node))
+            {
+                Node.Parent = null;
+                Node.UnloadContents();
+                _children.Remove(Node);
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            foreach (var child in _children)
+            {
+                child.Update(gameTime);
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            foreach (var child in _children)
+            {
+                child.DrawContent(GraphicsDevice, gameTime);
+            }
+        }
+
+    }
+}
